c113dd43c6930f90e59c02df3132bd2c16416b59
Josh Rodgers <bytemr@gmail.com>
2020-09-25 18:42:30 -0700

    Add a CLI for running a Dice REPL.

--- src/syntax/error.rs
+++ src/syntax/error.rs
@@ -12,3 +12,13 @@ pub enum SyntaxError {
     #[error("Anonymous function has too many arguments (max 255).")]
     AnonymousFnTooManyArguments(Span),
 }
+
+impl SyntaxError {
+    pub fn span(&self) -> Span {
+        match self {
+            SyntaxError::UnexpectedToken(token) => token.span(),
+            SyntaxError::FnTooManyArguments(_, span) => *span,
+            SyntaxError::AnonymousFnTooManyArguments(span) => *span,
+        }
+    }
+}
