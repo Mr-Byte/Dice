fe60b75e28792e725bf3281486d3e36435603be5
Josh Rodgers <bytemr@gmail.com>
2020-10-17 08:39:48 -0700

    Switch to an alternative string interning system that doens't require GC.

--- dice-core/src/value/string.rs
+++ dice-core/src/value/string.rs
@@ -1,33 +1,61 @@
-use gc::{Finalize, Gc, Trace};
+use gc::{Finalize, Trace};
+use std::collections::HashMap;
+use std::hash::BuildHasherDefault;
+use std::rc::Rc;
 use std::{
+    cell::RefCell,
     fmt::{Display, Formatter},
     hash::{Hash, Hasher},
     ops::Deref,
 };
+use wyhash::WyHash;
 
-#[derive(Debug, Clone, Trace, Finalize, PartialEq, Eq)]
+// NOTE: Use a naive string interning system for now.
+
+thread_local! {
+    static INTERNED_STRINGS: RefCell<HashMap<String, DString, BuildHasherDefault<WyHash>>> = Default::default();
+}
+
+#[derive(Debug, Clone, Trace, Finalize, Eq)]
 #[repr(transparent)]
 pub struct DString {
-    inner: Gc<String>,
+    #[unsafe_ignore_trace]
+    inner: Rc<str>,
 }
 
 impl From<String> for DString {
     fn from(value: String) -> Self {
-        Self { inner: value.into() }
+        INTERNED_STRINGS.with(|strings| {
+            strings
+                .borrow_mut()
+                .entry(value.clone())
+                .or_insert_with(|| Self { inner: value.into() })
+                .clone()
+        })
     }
 }
 
 impl<'a> From<&'a str> for DString {
     fn from(value: &'a str) -> Self {
-        Self {
-            inner: value.to_owned().into(),
-        }
+        INTERNED_STRINGS.with(|strings| {
+            strings
+                .borrow_mut()
+                .entry(value.to_owned().clone())
+                .or_insert_with(|| Self { inner: value.into() })
+                .clone()
+        })
+    }
+}
+
+impl PartialEq for DString {
+    fn eq(&self, other: &Self) -> bool {
+        self.as_ptr() == other.as_ptr()
     }
 }
 
 impl Hash for DString {
     fn hash<H: Hasher>(&self, state: &mut H) {
-        self.inner.as_str().hash(state)
+        self.inner.hash(state)
     }
 }
 
@@ -35,18 +63,18 @@ impl Deref for DString {
     type Target = str;
 
     fn deref(&self) -> &Self::Target {
-        self.inner.as_str()
+        &*self.inner
     }
 }
 
 impl AsRef<str> for DString {
     fn as_ref(&self) -> &str {
-        self.inner.as_str()
+        &*self.inner
     }
 }
 
 impl Display for DString {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.inner.as_str())
+        write!(f, "{}", self.inner)
     }
 }
