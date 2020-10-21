e75c590bc36c4b1b89ae09a78847cef2c23f8e75
Josh Rodgers <bytemr@gmail.com>
2020-10-21 14:16:21 -0700

    Add more mathematical operators to floats.

--- dice-runtime/src/classes/float.rs
+++ dice-runtime/src/classes/float.rs
@@ -17,11 +17,28 @@ pub fn register(runtime: &mut crate::Runtime<impl ModuleLoader>) {
     float.register_native_method(NEW, Rc::new(construct_float));
     float.register_native_method("abs", bind_f64(f64::abs));
     float.register_native_method("sqrt", bind_f64(f64::sqrt));
+    float.register_native_method("cbrt", bind_f64(f64::cbrt));
     float.register_native_method("floor", bind_f64(f64::floor));
     float.register_native_method("ceil", bind_f64(f64::ceil));
     float.register_native_method("round", bind_f64(f64::round));
     float.register_native_method("cos", bind_f64(f64::cos));
     float.register_native_method("sin", bind_f64(f64::sin));
+    float.register_native_method("tan", bind_f64(f64::tan));
+    float.register_native_method("acos", bind_f64(f64::acos));
+    float.register_native_method("asin", bind_f64(f64::asin));
+    float.register_native_method("atan", bind_f64(f64::atan));
+    float.register_native_method("atan2", bind_f64_2(f64::atan2));
+    float.register_native_method("cosh", bind_f64(f64::cosh));
+    float.register_native_method("sinh", bind_f64(f64::sinh));
+    float.register_native_method("tanh", bind_f64(f64::tanh));
+    float.register_native_method("acosh", bind_f64(f64::acosh));
+    float.register_native_method("asinh", bind_f64(f64::asinh));
+    float.register_native_method("atanh", bind_f64(f64::atanh));
+    float.register_native_method("log", bind_f64_2(f64::log));
+    float.register_native_method("log2", bind_f64(f64::log2));
+    float.register_native_method("ln", bind_f64(f64::ln));
+    float.register_native_method("log10", bind_f64(f64::log10));
+    float.register_native_method("pow", bind_f64_2(f64::powf));
 
     float.register_native_static_property("MAX", Value::Float(f64::MAX));
     float.register_native_static_property("MIN", Value::Float(f64::MIN));
@@ -54,6 +71,30 @@ fn bind_f64(
     Rc::new(move |_: &mut dyn Runtime, args: &[Value]| {
         if let [Value::Float(this), ..] = args {
             Ok(Value::Float(function(*this)))
+        } else {
+            Ok(Value::Null)
+        }
+    })
+}
+
+fn bind_f64_2(
+    function: impl Fn(f64, f64) -> f64 + 'static,
+) -> Rc<dyn Fn(&mut dyn Runtime, &[Value]) -> Result<Value, RuntimeError>> {
+    Rc::new(move |_: &mut dyn Runtime, args: &[Value]| {
+        if let [Value::Float(first), Value::Float(second), ..] = args {
+            Ok(Value::Float(function(*first, *second)))
+        } else {
+            Ok(Value::Null)
+        }
+    })
+}
+
+fn bind_f64_3(
+    function: impl Fn(f64, f64, f64) -> f64 + 'static,
+) -> Rc<dyn Fn(&mut dyn Runtime, &[Value]) -> Result<Value, RuntimeError>> {
+    Rc::new(move |_: &mut dyn Runtime, args: &[Value]| {
+        if let [Value::Float(first), Value::Float(second), Value::Float(third), ..] = args {
+            Ok(Value::Float(function(*first, *second, *third)))
         } else {
             Ok(Value::Null)
         }
