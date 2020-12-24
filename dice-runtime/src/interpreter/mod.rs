e2c36715469db077d38b35ac0a45fa74c5ad2527
Josh Rodgers <bytemr@gmail.com>
2020-12-24 13:22:36 -0800

    Add type assertion notes to all assertion types.

--- dice-runtime/src/interpreter/mod.rs
+++ dice-runtime/src/interpreter/mod.rs
@@ -200,17 +200,28 @@ where
             return Ok(());
         }
 
-        let is_type = value
+        let local_class = value
             .as_object()
             .ok()
             .and_then(|object| object.class())
-            .or_else(|| self.value_class_mapping.get(&value.kind()).cloned())
+            .or_else(|| self.value_class_mapping.get(&value.kind()).cloned());
+        let is_type = local_class
+            .as_ref()
             .map_or(false, |local_class| local_class.is_class(&class));
 
         if is_type {
             Ok(())
         } else {
-            Err(Error::new(TYPE_ASSERTION_FAILURE))
+            let expected_type = class.name().as_string();
+            let actual_type =
+                local_class.map_or(String::from("<unknown>"), |local_class| local_class.name().as_string());
+
+            Err(Error::new(TYPE_ASSERTION_FAILURE).push_context(
+                Context::new(MISMATCHED_TYPE_ASSERTIONS, ContextKind::Note).with_tags(tags! {
+                    expected => expected_type,
+                    actual => actual_type
+                }),
+            ))
         }
     }
 
@@ -223,17 +234,28 @@ where
             return Err(Error::new(TYPE_ASSERTION_NULLABILITY_FAILURE));
         }
 
-        let is_type = value
+        let local_class = value
             .as_object()
             .ok()
             .and_then(|object| object.class())
-            .or_else(|| self.value_class_mapping.get(&value.kind()).cloned())
-            .map_or(false, |return_class| return_class.is_class(&class));
+            .or_else(|| self.value_class_mapping.get(&value.kind()).cloned());
+        let is_type = local_class
+            .as_ref()
+            .map_or(false, |local_class| local_class.is_class(&class));
 
         if is_type {
             Ok(())
         } else {
-            Err(Error::new(TYPE_ASSERTION_FAILURE))
+            let expected_type = class.name().as_string();
+            let actual_type =
+                local_class.map_or(String::from("<unknown>"), |local_class| local_class.name().as_string());
+
+            Err(Error::new(TYPE_ASSERTION_FAILURE).push_context(
+                Context::new(MISMATCHED_TYPE_ASSERTIONS, ContextKind::Note).with_tags(tags! {
+                    expected => expected_type,
+                    actual => actual_type
+                }),
+            ))
         }
     }
 
@@ -246,17 +268,28 @@ where
             return Ok(());
         }
 
-        let is_type = value
+        let local_class = value
             .as_object()
             .ok()
             .and_then(|object| object.class())
-            .or_else(|| self.value_class_mapping.get(&value.kind()).cloned())
-            .map_or(false, |return_class| return_class.is_class(&class));
+            .or_else(|| self.value_class_mapping.get(&value.kind()).cloned());
+        let is_type = local_class
+            .as_ref()
+            .map_or(false, |local_class| local_class.is_class(&class));
 
         if is_type {
             Ok(())
         } else {
-            Err(Error::new(TYPE_ASSERTION_FAILURE))
+            let expected_type = class.name().as_string();
+            let actual_type =
+                local_class.map_or(String::from("<unknown>"), |local_class| local_class.name().as_string());
+
+            Err(Error::new(TYPE_ASSERTION_FAILURE).push_context(
+                Context::new(MISMATCHED_TYPE_ASSERTIONS, ContextKind::Note).with_tags(tags! {
+                    expected => expected_type,
+                    actual => actual_type
+                }),
+            ))
         }
     }
 
